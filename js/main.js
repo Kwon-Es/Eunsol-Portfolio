4e4d8acb376fd87fccbb0487ae11d2b4a08abfb6
Kwon-Es <waxo1@donga.ac.kr>
2022-09-22 14:56:29 +0900

    20220922

---
+++ js/main.js
@@ -0,0 +1,299 @@
+$(function(){ //opening
+    
+   
+    //1.Modal
+    //5.확인 버튼 클릭 시, modal 창 스르륵 사라지기
+    //eXmodal 체크 후, 버튼 클릭 시, modal 창 뜨지 않게 하기
+    if($.cookie('modal') == 'none'){
+        $('.modal').hide();
+        mainV();
+    };
+
+    function closeModal(){
+        if($('#eXmodal').is(':checked')){
+            $.cookie('modal','none',{expires:10000, path:'/'});
+        };
+        $('.modal').delay(400).fadeOut(300)
+
+        return
+    };
+
+    function mainV(){
+        $('.double').stop().animate({
+            bottom:0
+        }, 700)
+        $('.mvTxt h2').fadeIn(1000);
+        $('.mvTxt .bar').stop().animate({
+            height:10
+        },500);
+        $('.mainWords').stop().animate({
+            marginRight:0
+        }, 700)
+    };
+
+    $('.closeMD').on('click', function(){
+        $('.check').addClass('on');
+        closeModal();
+        setInterval(mainV, 700);
+    });
+
+
+    //2.Header
+    //2-1.openM 버튼 클릭 시
+    //#header bg-color 변경, border-btm, gnb, black 나타나기
+    //openM 숨기고 closeM 나타나기, gnb right 변경
+    //closeM 클릭 시 removeClass, gnb right 변경
+    $('.openM').on('click', function(){
+        $(this).addClass('on');
+        $('#header, .gnb, .black, .closeM').addClass('on');
+        $('.gnb').stop().animate({
+            right:102
+        });
+    });
+    $('.closeM').on('click', function(){
+        $(this).removeClass('on')
+        $('#header, .gnb, .black, .openM').removeClass('on');
+        $('.gnb').stop().animate({
+            right:0
+        });
+    });
+
+    //2-2. 스크롤 발생 시 동작
+    //메뉴영역 나타나고 사라지기
+    $(window).on('scroll', function(){
+        let scr = $(this).scrollTop();
+
+        if(scr > 0){
+            $('#header, .gnb, .black, .openM').addClass('on');
+            $('.closeM').removeClass('on');
+            $('.gnb').stop().animate({
+                right:0
+            });
+        }else{
+            $('#header, .gnb, .black, .openM, closeM').removeClass('on');
+        };
+    });
+    
+    //2-3.gnb li 클릭시 scroll위치 변경, color 변경
+    let n1 = $('.gnb li:nth-child(1)')
+    let n2 = $('.gnb li:nth-child(2)')
+    let n3 = $('.gnb li:nth-child(3)')
+    let n4 = $('.gnb li:nth-child(4)')
+    let n5 = $('.gnb li:nth-child(5)')
+    let winHeight = $(window).height();
+
+    $(n1).on('click', function(){
+        $(this).addClass('on').siblings().removeClass();
+        $('html,body').animate({
+            scrollTop:`${winHeight}`
+        });
+    });
+    $(n2).on('click', function(){
+        $(this).addClass('on').siblings().removeClass();
+        $('html,body').animate({
+            scrollTop:`${winHeight*2}`
+        })
+    });
+    $(n3).on('click', function(){
+        $(this).addClass('on').siblings().removeClass();
+        $('html,body').animate({
+            scrollTop:`${winHeight*4}`
+        })
+    });
+    $(n4).on('click', function(){
+        $(this).addClass('on').siblings().removeClass();
+        $('html,body').animate({
+            scrollTop:`${winHeight*6}`
+        })
+    });
+    $(n5).on('click', function(){
+        $(this).addClass('on').siblings().removeClass();
+        $('html,body').animate({
+            scrollTop:$(document).height()
+        });
+    })
+
+    //2-4.스크롤 최상단일 때, 메인메뉴 color 변경
+    $(window).on('scroll', function(){
+        let scr = $('html, body').scrollTop();
+        
+        if(scr == 0){
+            $('.gnb li').removeClass('on');
+        };
+    });
+
+    //2-5.로고 클릭 시, 새로고침 되기
+    $('.logo').on('click', function(e){
+        e.preventDefault();
+        location.reload();
+    });
+
+
+    //4.About
+    //4-1.스크롤 되면 clickS, 스르륵 나타나기
+    $(window).on('scroll',function(){
+        let winHeight = $(this).height();
+        let scr = $('html, body').scrollTop();
+
+        if(scr == winHeight*1){
+            $('.clickS').addClass('on')
+        } else{
+            $('.clickS').removeClass('on')
+        };
+    });
+
+    //4-2.l5 icon li 클릭 시
+    //클릭한 li opacity, 순서에 맞는 sTxt 텍스트 나타나기, clickS 숨기기
+    //다시 클릭 시, opacity, sTxt 텍스트 숨기기
+    let s1 = $('.s1 .icon li')
+    let s2 = $('.s2 .icon li')
+    let st1 = $('.st1 li')
+    let st2 = $('.st2 li')
+
+    $(s1).on('click', function(){
+        let gi = $(this).index();
+
+            $(this).toggleClass('on').siblings().removeClass('on');
+            $(s2).removeClass('on');
+            $(st1).eq(gi).toggle().siblings().hide();
+            $(st2).hide();
+            $('.clickS').removeClass('on')
+    });
+
+    $(s2).on('click', function(){
+        let ei = $(this).index();
+
+            $(this).toggleClass('on').siblings().removeClass('on');
+            $(s1).removeClass('on');
+            $(st2).eq(ei).toggle().siblings().hide();
+            $(st1).hide();
+            $('.clickS').removeClass('on')
+    });
+
+
+    //5.Container
+    //5-1.특정 위치에 스크롤 됐을 때, text영역 나타나기
+    //해당 위치를 벗어났을 때, text 영역 사라지기
+    $(window).on('scroll', function(){
+        let scr = $(this).scrollTop();
+        let winHeight = $(this).height();
+        i = 2;
+
+        if(scr == winHeight*i){
+            $('.resp1 .right').addClass('on');
+        } else{
+            $('.resp1 .right').removeClass('on');
+        };
+
+        if(scr == winHeight*(i+1)){
+            $('.resp2 .left').addClass('on');
+        } else{
+            $('.resp2 .left').removeClass('on');
+        };
+        
+        if(scr == winHeight*(i+2)){
+            $('.stan1 .right').addClass('on');
+        } else{
+            $('.stan1 .right').removeClass('on');
+        };
+
+        if(scr == winHeight*(i+3)){
+            $('.stan2 .left').addClass('on');
+        } else{
+            $('.stan2 .left').removeClass('on');
+        };
+
+        if(scr >= winHeight*(i+4)){
+            $('.mob .right').addClass('on');
+        } else{
+            $('.mob .right').removeClass('on');
+        };
+    });
+
+
+    //5-2.스크롤 되면 hoverS, 스르륵 나타나기
+    $(window).on('scroll',function(){
+        let winHeight = $(this).height();
+        let scr = $('html, body').scrollTop();
+
+        if(scr == winHeight*2){
+            $('.hoverS').addClass('on')
+        } else{
+            $('.hoverS').removeClass('on')
+        };
+    });
+
+    //5-3.screen에 mouseover 되었을 때, hoverS 스르륵 사라지기
+    $('.screen').on('mouseenter', function(){
+        $('.hoverS').removeClass('on')
+    })
+
+
+    //6.Window
+    //6-1.마우스 휠 스크롤 시, 100vh 씩 이동
+    $('section').on('mousewheel', function(e, d){
+        e.preventDefault();
+
+        if(d > 0){
+            let prv = $(this).prev().offset().top;
+            $('html, body').stop().animate({
+                scrollTop:prv
+            });
+        } else if(d < 0){
+            let nxt = $(this).next().offset().top;
+            $('html, body').stop().animate({
+                scrollTop:nxt
+            })
+        }
+    });
+       
+    //6-2.새로고침 시, 최상단으로 이동
+    function top(){
+        $('html, body').animate({
+               scrollTop:0
+           }, 500);
+       };
+       top();
+
+
+    //7.Footer
+    //slBox 구현
+    //셀박 클릭 시, 스타일 변경, option 나타내기, 아이콘 변경
+    $('.slBox').on('click', function(){
+        $('.slBox').toggleClass('on');
+        $('.sList').stop().toggleClass('on');
+        $('.sIco').stop().toggleClass('on');
+    });
+
+    //선택한 li의 텍스트를 박스 안에 나타내기
+    $('.sList li').on('click', function(){
+        let sTxt = $(this).text();
+
+        $('.selected').text(sTxt);
+    });
+
+
+    //8.Go
+    //8-1.일정값 이상 스크롤했을 때 go버튼 스르륵 나타나기
+    $(window).on('scroll',function(){
+        let scr = $('html, body').scrollTop();
+
+        if( scr > 500 ){
+            $('.go').addClass('on')
+        } else{
+            $('.go').removeClass('on')
+        };
+    });
+
+    //8-2.goTop 버튼 클릭 시, 최상단으로 이동
+    $('.goTop').on('click', function(){
+        window.scrollTo({top:0, behavior:"smooth"});
+    });
+
+    //8-3.goBtm 버튼 클릭 시, 최하단으로 이동
+    $('.goBtm').on('click', function(){
+        window.scrollTo({top:10000, behavior:"smooth"});
+    });
+
+
+}); //end
---
+++ web/Emons/js/main.js
@@ -0,0 +1,302 @@
+$(function(){
+
+
+    //1.Header
+    let winWidth = $(window).width();
+
+    if(winWidth < 1200){
+        //1-1. 버튼 클릭 시 메뉴영역 나타나고 사라지기
+        $('.openM').on('click', function(){
+            $('.menu').stop().animate({
+                left:0
+            }), 400;
+        });
+        $('.closeM').on('click', function(){
+            $('.menu').stop().animate({
+                left:'-100%'
+            }), 400;
+        });
+
+        //1-2. 메인메뉴 클릭 시, 서브메뉴1 나타내기
+        $('.depth1 h3').on('click', function(){
+            $(this).siblings().stop().slideToggle(400);
+            $(this).parent().siblings().find('.depth2').stop().slideUp(400);
+        });
+        //1-3. 서브메뉴1 클릭 시, 서브메뉴2 나타내기\
+        $('.depth2>li p').on('click', function(){
+            $(this).siblings().stop().slideToggle(400);
+            $(this).parent().siblings().find('.depth3').stop().slideUp(400);
+            //arrow 아이콘 회전
+            $(this).toggleClass('on').parent().siblings().find('p').removeClass('on');
+        });
+    } else {
+         //1-1. 메인메뉴 hover 시, 서브메뉴1 나타나고 사라지기
+         $('.depth1 h3').on('mouseenter', function(){
+            $(this).siblings().stop().show();
+            $(this).parent().siblings().find('.depth2').stop().hide();
+            $('.subBg').stop().show();
+        });
+        $('.depth1').on('mouseleave', function(){
+            $(this).find('.depth2').stop().hide();
+            $('.subBg').stop().hide();
+        })
+
+        //1-2. 서브메뉴1 hover 시, 서브메뉴2 나타내기, 아이콘 회전
+        $('.depth2>li p').on('mouseenter', function(){
+            $(this).siblings().stop().show();
+            $(this).parent().siblings().find('.depth3').stop().hide();
+            $(this).children('.isub').css({
+                transform : `rotate(0deg)`,
+                paddingRight : `0px`
+            }).parents('li').siblings().find('.isub').css({
+                transform : `rotate(90deg)`,
+                paddingRight : `2px`
+            });
+        });
+        $('.depth2').on('mouseleave', function(){
+            $(this).find('.depth3').stop().hide();
+            $(this).find('.isub').css({
+                transform : `rotate(90deg)`,
+                paddingRight : `2px`
+            });
+        });
+
+
+        //1-3. 아이콘 클릭 시, 써치박스 나타내기
+        $('.isc').on('click', function(){
+            $('.scBox').stop().toggle();
+        });
+    
+    };
+
+
+    //2.Mainvisual
+    //(all)
+    //메인비주얼 이미지 자동 슬라이드
+    $('.simg').slick({
+        slidesToShow:1,//화면에서 보여질 이미지 개수
+        slidesToScroll:1, //스크를시 이동할 이미지 개수
+        arrows:false,//기본값, 사용함
+        dots:true, //pager 사용 여부, 기본값:false
+        speed:700,//다음페이지로 넘어가는 시간
+        autoplay:true, //자동재생 여부, 포트폴리오에서는 항상 켜놓기
+        autoplaySpeed:1200, //자동재생시 이미지가 유지되는 시간
+        pauseOnhHover:false,//기본값, 마우스 hover시 자동재생 멈춤 여부
+        pauseOnFocus: false,
+        infinite:true,//무한반복 여부, 마지막 요소>>첫번째 요소
+        vertical:false//세로 슬라이드   
+    });
+
+
+    //3.Curation
+    //3-1.(360~767px) 큐레이션 이미지 자동 슬라이드
+    $('.cimgM').stop().slick({
+        responsive:[
+            {
+                breakpoint : 20000,//768~
+                settings : "unslick"
+            },
+            {
+                breakpoint : 768,//360~767
+                settings : {
+                    slidesToShow:1,
+                    slidesToScroll:1,
+                    arrows:false,
+                    dots:true,
+                    speed:700,
+                    autoplay:true,
+                    autoplaySpeed:1200,
+                    pauseOnhHover:false,
+                    pauseOnFocus: false,
+                    infinite:true,
+                    vertical:false
+                }
+            }
+        ]
+    });
+    
+    //3-2.(768px~) 큐레이션 이미지 자동 슬라이드
+    $('.cimgPC').stop().slick({
+        slidesToShow:1,
+        slidesToScroll:1,
+        arrows:true,
+        dots:false,
+        speed:700,
+        autoplay:true,
+        autoplaySpeed:1200,
+        pauseOnhHover:false,
+        pauseOnFocus: false,
+        infinite:true,
+        vertical:false, //1200~
+        responsive:[
+            {
+                breakpoint : 768,//360~767
+                settings : "unslick"
+            }
+        ]
+    });
+
+    //3-3.(768px~) 큐레이션 Pager 탭메뉴
+    $('.curPager li').on('click', function(){
+        let i = $(this).index();
+        
+        $(this).addClass('on').siblings().removeClass('on');
+        $('.curR>li').eq(i).addClass('on').siblings().removeClass('on');
+        $('.curL>li').eq(i).addClass('on').siblings().removeClass('on');
+        $('.cimgPC').slick('setPosition'); //slick 대상의 자리를 다시 잡아줌
+    });
+
+
+    //4.Promotion
+    //4-1.(all) 드라마 포스터 자동 슬라이드
+    $('.dList').slick({
+        slidesToShow:4,
+        slidesToScroll:2,
+        arrows:true,
+        dots:false,
+        speed:700,
+        autoplay:true,
+        autoplaySpeed:1200,
+        pauseOnhHover:false,
+        pauseOnFocus: false,
+        infinite:false,
+        vertical:false, //1200~
+        responsive:[
+            {
+                breakpoint : 1200,//640~1199
+                settings : {
+                slidesToShow:3,
+                slidesToScroll:2
+                }
+            },
+            {
+                breakpoint : 640,//360~639
+                settings : {
+                slidesToShow:2,
+                slidesToScroll:1
+                }
+            }
+        ]
+    });
+
+    //4-2.(360~767px) 셀러브리티 이미지 자동 슬라이드
+    $('.celebM').stop().slick({
+        responsive:[
+            {
+                breakpoint : 20000,//768~
+                settings : "unslick"
+            },
+            {
+                breakpoint : 768,//360~767
+                settings : {
+                    slidesToShow:2,
+                    slidesToScroll:1,
+                    arrows:true,
+                    dots:false,
+                    speed:700,
+                    autoplay:true,
+                    autoplaySpeed:1200,
+                    pauseOnhHover:false,
+                    pauseOnFocus: false,
+                    infinite:true,
+                    vertical:false
+                }
+            }
+        ]
+    });
+
+    //4-3.셀러브리티 Pager 탭메뉴
+    $('.clPager li').on('click', function(){
+        let i = $(this).index();
+
+        $(this).addClass('on').siblings().removeClass('on');
+        $('.celebR>div').eq(i).addClass('on').siblings().removeClass('on');
+        $('.celebL>li').eq(i).addClass('on').siblings().removeClass('on');
+    });
+
+
+    //5.Review
+    //5-1. 리뷰 자동 슬라이드
+    $('.rWrap').stop().slick({
+        slidesToShow:4,
+        slidesToScroll:4,
+        arrows:false,
+        dots:false,
+        speed:700,
+        autoplay:true,
+        autoplaySpeed:1200,
+        pauseOnhHover:false,
+        pauseOnFocus: false,
+        infinite:true,
+        vertical:false,
+        responsive:[
+            {
+                breakpoint : 1200,//640~1199
+                settings : {
+                    slidesToShow:3,
+                    slidesToScroll:3
+                }
+            },
+            {
+                breakpoint : 640,//360~639
+                settings : {
+                    slidesToShow:2,
+                    slidesToScroll:1
+                }
+            }
+        ]
+    });
+
+    //타이틀 탭메뉴
+    $('.tabM div').on('click', function(){
+        let i = $(this).index();
+        
+        $(this).addClass('on').siblings().removeClass('on');
+        $('.rList>li').eq(i).addClass('on').siblings().removeClass('on');
+        $('.rWrap').slick('setPosition');
+    });
+
+
+    //6.(all) CS 영역 VR 이미지 수동 슬라이드
+    $('.vrR').slick({
+        slidesToShow:1,
+        slidesToScroll:1,
+        arrows:true,
+        dots:false,
+        speed:700,
+        autoplay:false,
+        autoplaySpeed:1200,
+        pauseOnhHover:false,
+        pauseOnFocus: false,
+        infinite:true,
+        vertical:false
+    });
+    
+    //7.window
+    $(window).on('resize', function(){
+        document.location.reload();
+    });
+
+
+    //8.goTop
+    //8-1.일정값 이상 스크롤했을 때 goTop버튼 스르륵 나타나기
+    $(window).on('scroll',function(){
+        let scr = $('html, body').scrollTop();
+
+        if( scr > 1100 ){
+            $('.goTop').addClass('on')
+        } else{
+            $('.goTop').removeClass('on')
+        };
+    });
+
+    //8-2.goTop 버튼 클릭 시, 최상단으로 이동
+    $('.goTop').on('click', function(){
+        window.scrollTo({top:0, behavior:"smooth"});
+    });
+
+     
+
+
+
+}); //opening
